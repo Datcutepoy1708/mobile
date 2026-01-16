9f626566cfd4a3e563bcb259a96019b776360302
Trong Dat <dtcutepoy1708@gmail.com>
2026-01-16 09:41:59 +0700

    bai 31

--- App.tsx
+++ App.tsx
@@ -6,7 +6,17 @@ import { TextInput } from 'react-native';
 export default function App() {
   //string
   const [name,setName]=useState<string>("");
-
+  const [toDoList,setToDoList]=useState([
+    {id:1,title:"Learn React Native"},
+    {id:2,title:"Learn React JS"},
+    {id:3,title:"Watching Netflix"},
+    {id:4,title:"Watching Youtube"},
+    {id:5,title:"Watching Film"},
+    {id:6,title:"Độ Mixi"},
+    {id:7,title:"Mẹ Lý"},
+    {id:8,title:"Con chó Cao bằng"},
+    {id:9,title:"Khô gà loại 2"}
+  ])
 
   //number
   // const[age,setAge]=useState<number>(30);
@@ -30,7 +40,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.text}>{name}</Text>
         <TextInput
         onChangeText={ v => setName(v)}
         value={name}
@@ -40,32 +49,49 @@ export default function App() {
         multiline={true}
         autoCorrect={false}
         style={{
+          marginTop:50,
           borderColor:"violet",
           borderWidth:1,
           padding:10
         }}/>
         {/* <Text style={styles.text}>{age}</Text>
         <Text style={styles.text}>{person.name}</Text> */}
-
+      <Text style={styles.text}>{name}</Text>
       </View>
-      <Text style={styles.text}>Hello world with 
+      {/* <Text style={styles.text}>Hello world with 
         <Text style={styles.datcutepoy}>
            datcutepoy
         </Text>
-      </Text>
-      <Button title='add new' />
+      </Text> */}
+      <View style={{marginTop:20,  borderColor:"red",borderWidth:1}}>
+        {toDoList.map(todo => {
+          return (
+            <Text key={todo.id} style={styles.todo}>{todo.title}</Text>
+          )
+        })}
+      </View>
+      <Button title='add new' color={"green"} 
+      onPress={()=>alert("Click me")}
+      />
     </View>
     
   );
 }
 
 const styles = StyleSheet.create({
+  todo: {
+   fontSize:30,
+   backgroundColor:"pink",
+   marginBottom:10,
+   padding:15
+  },
   datcutepoy: {
     color:'green'
   },
   text: {
     fontSize:20,
-    color:'red'
+    color:'red',
+    marginTop:30
   },
   container: {
     flex: 1,
